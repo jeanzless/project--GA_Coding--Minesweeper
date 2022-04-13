3b244f92a711cd75e3936fc61c27e9c8ae01158d
jeanzless <jeanzle.albalate208@gmail.com>
2022-04-13 11:47:18 +0100

    grid and bomb recognition commit

--- app.js
+++ app.js
@@ -1,30 +1,113 @@
 const grid = document.querySelector('.grid');
-const width = 8;
-const totalCells = width * width;
 const gridList = document.querySelectorAll('.grid div');
-const main = document.querySelectorAll('body');
-let mode = null;
-console.log(grid);
-console.log(gridList);
 
-function createGrid() {
-  for (let i = 0; i < totalCells; i++){
+const leftWall = [8, 16, 24, 32, 40, 48];
+const arrayGrid = [];
+const rightWall = [7];
 
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    cell.textContent = i + 1;
-    grid.appendChild(cell);
-    cell.addEventListener('click', () => {
-      console.log(cell.innerHTML);
-    })
 
+gridList.forEach((i) => {
+  arrayGrid.push(i);
+  i.textContent = arrayGrid[i];
+
+  i.addEventListener(('click'), (event) => {
+    const bombs = document.querySelector('#bomb');
+    if (event.target.id === 'bomb'){
+
+      console.log('BOOM!!');
+      console.log(arrayGrid[i]);
+
+    } else if (event.target.class === 'cell' && !event.target.id === 'bomb'){
+
+      console.log('empty Cell');
+
+    }
+  })
+})
+
+for (let i = 0; i < 8; i++){
+  var ranBomb = arrayGrid[Math.floor(Math.random() * arrayGrid.length)];
+  ranBomb.setAttribute('id','bomb');
+}
+
+const leftWalls = leftWall.filter(number => {
+  if (arrayGrid[number].id === 'bomb'){
+    return number;
   }
-  console.log(gridList);
+})
+
+console.log(leftWalls)
+
+for (let i = 0; i < arrayGrid.length; i++) {
+  if (arrayGrid[i].id === 'bomb'){
+    // arrayGrid[i + 1].setAttribute('id', 'nextTo');
+    // arrayGrid[i + 8].setAttribute('id', 'nextTo');
+    // arrayGrid[i + 7].setAttribute('id', 'nextTo');
+    // arrayGrid[i + 9].setAttribute('id', 'nextTo');
+    // arrayGrid[i - 1].setAttribute('id', 'nextTo');
+    // arrayGrid[i - 8].setAttribute('id', 'nextTo');
+    // arrayGrid[i - 7].setAttribute('id', 'nextTo');
+    // arrayGrid[i - 9].setAttribute('id', 'nextTo');
+    
+  }
+  leftWalls.map(number => {
+    if (number === i){
+      if (arrayGrid[i - 8].id !== 'bomb' || arrayGrid[i - 7].id !== 'bomb' || arrayGrid[i + 1].id !== 'bomb' || arrayGrid[i + 8].id !== 'bomb' || arrayGrid[i + 9].id !== 'bomb') {
+        console.log(arrayGrid[i]);
+        arrayGrid[i - 8].setAttribute('id', 'nextTo');
+        arrayGrid[i - 7].setAttribute('id', 'nextTo');
+        arrayGrid[i + 1].setAttribute('id', 'nextTo');
+        arrayGrid[i + 8].setAttribute('id', 'nextTo');
+        arrayGrid[i + 9].setAttribute('id', 'nextTo');
+        arrayGrid[i].setAttribute('id', 'bomb');
+      }
+    }
+  })
+
 }
+// for (let i = 0; i < 8; i++){
+//   var ranBomb = arrayGrid[Math.floor(Math.random() * arrayGrid.length)];
+//   console.log(ranBomb);
+//   ranBomb.setAttribute('id','bomb');
+// }
 
-function startGame(mode){
 
-  createGrid();
 
-}
-startGame('');
+
+
+
+
+
+
+
+
+
+
+// function createGrid() {
+//   for (let i = 0; i < totalCells; i++){
+
+//     const cell = document.createElement('div');
+//     cell.classList.add('cell');
+//     arrayGrid.push(i);
+//     cell.textContent = i + 1;
+//     grid.appendChild(cell);
+//     cell.addEventListener('click', () => {
+//       console.log(cell.innerHTML);  
+//     })
+//   }
+//   for (let i = 0; i < 5; i++){
+//     var ranBomb = arrayGrid[Math.floor(Math.random() * arrayGrid.length)];
+//     console.log(ranBomb);
+//     console.log(arrayGrid[ranBomb])
+//   }
+// }
+
+
+
+
+// function startGame(){
+
+//   createGrid();
+
+// }
+// startGame('');
