82e25ed4af14968a2ed787b1f3e8e25719c89069
jeanzless <jeanzle.albalate208@gmail.com>
2022-04-14 20:59:18 +0100

    walls are given bomb detection

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const grid = document.querySelector('.grid');
 const gridList = document.querySelectorAll('.grid div');
 
 const leftWall = [8, 16, 24, 32, 40, 48];
+const bombGrid = [];
 const arrayGrid = [];
-const rightWall = [7];
+const rightWall = [15, 23, 31, 39, 47, 55];
+const topWall = [1, 2, 3, 4, 5, 6]
+const bottomWall = [57, 58, 59, 60, 61, 62];
 
 
 gridList.forEach((i) => {
@@ -15,7 +18,7 @@ gridList.forEach((i) => {
     if (event.target.id === 'bomb'){
 
       console.log('BOOM!!');
-      console.log(arrayGrid[i]);
+      console.log(arrayGrid[i]);                      //CLICK EVENTS
 
     } else if (event.target.class === 'cell' && !event.target.id === 'bomb'){
 
@@ -27,6 +30,8 @@ gridList.forEach((i) => {
 
 for (let i = 0; i < 8; i++){
   var ranBomb = arrayGrid[Math.floor(Math.random() * arrayGrid.length)];
+  bombGrid.push(ranBomb);    //CHOOSES RANDOM BOMBS WITHIN THE GRID
+  console.log(bombGrid);
   ranBomb.setAttribute('id','bomb');
 }
 
@@ -36,29 +41,69 @@ const leftWalls = leftWall.filter(number => {
   }
 })
 
-console.log(leftWalls)
+const rightWalls = rightWall.filter(number => {
+  if (arrayGrid[number].id === 'bomb'){
+    return number;
+  }
+})
+
+const topWalls = topWall.filter(number => {
+  if (arrayGrid[number].id === 'bomb'){
+    return number;
+  }
+})
+
+const bottomWalls = bottomWall.filter(number => {
+  if (arrayGrid[number].id === 'bomb'){
+    return number;
+  }
+})
 
 for (let i = 0; i < arrayGrid.length; i++) {
-  if (arrayGrid[i].id === 'bomb'){
-    // arrayGrid[i + 1].setAttribute('id', 'nextTo');
-    // arrayGrid[i + 8].setAttribute('id', 'nextTo');
-    // arrayGrid[i + 7].setAttribute('id', 'nextTo');
-    // arrayGrid[i + 9].setAttribute('id', 'nextTo');
-    // arrayGrid[i - 1].setAttribute('id', 'nextTo');
-    // arrayGrid[i - 8].setAttribute('id', 'nextTo');
-    // arrayGrid[i - 7].setAttribute('id', 'nextTo');
-    // arrayGrid[i - 9].setAttribute('id', 'nextTo');
-    
-  }
   leftWalls.map(number => {
     if (number === i){
       if (arrayGrid[i - 8].id !== 'bomb' || arrayGrid[i - 7].id !== 'bomb' || arrayGrid[i + 1].id !== 'bomb' || arrayGrid[i + 8].id !== 'bomb' || arrayGrid[i + 9].id !== 'bomb') {
-        console.log(arrayGrid[i]);
         arrayGrid[i - 8].setAttribute('id', 'nextTo');
         arrayGrid[i - 7].setAttribute('id', 'nextTo');
         arrayGrid[i + 1].setAttribute('id', 'nextTo');
         arrayGrid[i + 8].setAttribute('id', 'nextTo');
         arrayGrid[i + 9].setAttribute('id', 'nextTo');
+        arrayGrid[i].setAttribute('id', 'bomb');
+      } 
+    }
+  })
+  rightWalls.map(number => {
+    if (number === i){
+      if (arrayGrid[i - 8].id !== 'bomb' || arrayGrid[i - 9].id !== 'bomb' || arrayGrid[i - 1].id !== 'bomb' || arrayGrid[i + 8].id !== 'bomb' || arrayGrid[i + 7].id !== 'bomb') {
+        arrayGrid[i - 9].setAttribute('id', 'nextTo');
+        arrayGrid[i - 8].setAttribute('id', 'nextTo');
+        arrayGrid[i - 1].setAttribute('id', 'nextTo');
+        arrayGrid[i + 8].setAttribute('id', 'nextTo');
+        arrayGrid[i + 7].setAttribute('id', 'nextTo');
+        arrayGrid[i].setAttribute('id', 'bomb');
+      }
+    }
+  })
+  topWalls.map(number => {
+    if (number === i){
+      if (arrayGrid[i + 8].id !== 'bomb' || arrayGrid[i - 1].id !== 'bomb' || arrayGrid[i + 7].id !== 'bomb' || arrayGrid[i + 9].id !== 'bomb' || arrayGrid[i + 1].id !== 'bomb'){
+        arrayGrid[i + 1].setAttribute('id', 'nextTo');
+        arrayGrid[i + 7].setAttribute('id', 'nextTo');
+        arrayGrid[i + 8].setAttribute('id', 'nextTo');
+        arrayGrid[i + 9].setAttribute('id', 'nextTo');
+        arrayGrid[i - 1].setAttribute('id', 'nextTo');
+        arrayGrid[i].setAttribute('id', 'bomb');
+      }
+    }
+  })
+  bottomWalls.map(number => {
+    if (number === i){
+      if (arrayGrid[i + 1].id !== 'bomb' || arrayGrid[i - 1].id !== 'bomb' || arrayGrid[i - 8].id !== 'bomb' || arrayGrid[i - 7].id !== 'bomb' || arrayGrid[i - 9].id !== 'bomb'){
+        arrayGrid[i + 1].setAttribute('id', 'nextTo');
+        arrayGrid[i - 7].setAttribute('id', 'nextTo');
+        arrayGrid[i - 8].setAttribute('id', 'nextTo');
+        arrayGrid[i - 9].setAttribute('id', 'nextTo');
+        arrayGrid[i - 1].setAttribute('id', 'nextTo');
         arrayGrid[i].setAttribute('id', 'bomb');
       }
     }
